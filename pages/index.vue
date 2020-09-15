0b2c0b12dd99d36d8389ac0a7479a761425c0003
Ben <allobon@gmail.com>
2020-09-15 16:28:49 +0100

    make a basic page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,34 @@
 <template lang="html">
   <main class="center">
     <site-logo></site-logo>
-    <section class="row mbx">
-      <input placeholder="Enter URL" v-model="url"/>
-      <button class="mt text-center" @click="handleForm">LET RIP</button>
-    </section>
+
+    <h1>Sites</h1>
+    <div class="row">
+      <div class="c50">
+        <h3>Deezer</h3>
+        <button>Connect</button>
+      </div>
+      <div class="c50">
+        <h3>Github</h3>
+        <button>Connect</button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="c50">
+        <h3>Lastfm</h3>
+        <button>Connect</button>
+      </div>
+      <div class="c50">
+        <h3>Spotify</h3>
+        <button>Connect</button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="c50">
+        <h3>YouTube</h3>
+        <button>Connect</button>
+      </div>
+    </div>
   </main>
 </template>
 
